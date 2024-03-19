d987a8cd58826ddb066cf0b00ad260bbc03472d6
bestrookie <491051587@qq.com>
2024-03-19 15:10:28 +0800

    add: aop 扩展到 bean 生命周期 项目初始化

---
+++ spring-13/src/main/java/com/bestrookie/springframework/dto/UserDao.java
@@ -0,0 +1,29 @@
+package com.bestrookie.springframework.dto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author bestrookie
+ * @Date 2023/12/12 10:36
+ * @Desc
+ */
+public class UserDao {
+    private static Map<String, String> hasMap = new HashMap<>();
+
+    public void initDataMethod(){
+        System.out.println("执行：init-method");
+        hasMap.put("10001", "小傅哥");
+        hasMap.put("10002", "八杯水");
+        hasMap.put("10003", "阿毛");
+    }
+
+    public String queryUserName(String uId){
+        return hasMap.get(uId);
+    }
+
+    public void destroyDataMethod(){
+        System.out.println("执行：destroy-method");
+        hasMap.clear();
+    }
+}
---
+++ spring-13/src/main/java/com/bestrookie/springframework/dto/UserService.java
@@ -0,0 +1,36 @@
+package com.bestrookie.springframework.dto;
+
+
+import java.util.Random;
+
+/**
+ * @Author bestrookie
+ * @Date 2023/12/12 10:41
+ * @Desc
+ */
+public class UserService implements IUserService {
+    @Override
+    public String queryUserInfo() {
+        try {
+            Thread.sleep(new Random(1).nextInt(100));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "bestrookie, 1000001, 青岛";
+    }
+
+    @Override
+    public String register(String userName) {
+        try {
+            Thread.sleep(new Random(1).nextInt(100));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "注册用户： " + userName + "success";
+    }
+
+    @Override
+    public String helloWorld() {
+        return "不走监控";
+    }
+}
